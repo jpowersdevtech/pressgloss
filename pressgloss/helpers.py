98b398cd8e68ea2ad9fee8b34a269d7ac19e59fa
Ben Johnson <bkj.322@gmail.com>
2022-10-13 11:49:20 -0400

    Update helpers.py

--- pressgloss/helpers.py
+++ pressgloss/helpers.py
@@ -44,7 +44,7 @@ def coastalize(instr): # type: (str) -> str
   """
 
   retstr = instr
-  retstr = re.sub(r'([A-Z]{3})([ENS])CS', r'(\1 \2S)', retstr)
+  retstr = re.sub(r'([A-Z]{3})([ENS]C)S', r'(\1 \2)', retstr)
 
   return retstr
 
